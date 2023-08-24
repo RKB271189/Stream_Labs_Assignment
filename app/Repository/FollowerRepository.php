2043b2dd78bba4c48deaf42eac5ca0356151f97b
Rahul B <rahul.b@avdevs.com>
2023-08-24 13:30:23 -0600

    Adding total on dashboard

--- app/Repository/FollowerRepository.php
+++ app/Repository/FollowerRepository.php
@@ -2,10 +2,19 @@
 
 namespace App\Repository;
 
+use App\Models\Follower;
 
-class FollowerRepository implements ServiceInterface{
+class FollowerRepository implements ServiceInterface
+{
+    private $table;
+    public function __construct(Follower $follower)
+    {
+        $this->table = $follower;
+    }
     public function getTotal(string $startDate, string $endDate)
     {
-        
+        $details = $this->table->whereBetween('created_at', [$startDate, $endDate])
+            ->count();
+        return $details;
     }
 }
--- app/Repository/SubscriberRepository.php
+++ app/Repository/SubscriberRepository.php
@@ -2,9 +2,26 @@
 
 namespace App\Repository;
 
+use App\Models\Subscriber;
+
 class SubscriberRepository implements ServiceInterface
 {
+    private $table;
+    public function __construct(Subscriber $subscriber)
+    {
+        $this->table = $subscriber;
+    }
     public function getTotal(string $startDate, string $endDate)
     {
+        $details = $this->table->whereBetween('created_at', [$startDate, $endDate])
+            ->selectRaw('subscription_tier, SUM(CASE
+                        WHEN subscription_tier = 1 THEN 5
+                        WHEN subscription_tier = 2 THEN 10
+                        WHEN subscription_tier = 3 THEN 15
+                        ELSE 0
+                    END) AS amount')
+            ->groupBy('subscription_tier')
+            ->get();
+        return $details;
     }
 }
